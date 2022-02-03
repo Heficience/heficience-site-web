fc6233e7f25dc488b1fd932518d242ff5caee258
Paullux <paulwoisard@gmail.com>
2022-02-03 13:38:44 +0100

    Ajout Facebook Pixel

--- index.php
+++ index.php
@@ -90,6 +90,25 @@ function getCountTableName($tableName)
     <meta name="twitter:description" content="Un groupe de développeurs créé pour aider les personnes en situation de handicap en développant des applications pour ordinateur ou mobile." />
     <meta name="twitter:image" content="https://heficience.com/assets/img/Carte-Twitter.png" />
     <meta name="twitter:image:alt" content="Heficience Logo" />
+
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1673451336336178');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1"
+             src="https://www.facebook.com/tr?id=1673451336336178&ev=PageView&noscript=1"/>
+    </noscript>
+    <!-- End Facebook Pixel Code -->
 <!--     Fonts and icons     -->
 <link rel="styleshesvet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
 
--- pages/contact-us.php
+++ pages/contact-us.php
@@ -114,7 +114,7 @@ if (isset($_POST["submit"])) {
         $response = curl_exec( $ch );
         curl_close( $ch );
 
-        $messageWarning='Formulaire envoyé à Heficience';
+        $messageWarning='Formulaire envoyé à Heficience, Merci pour votre participation.';
         $colorMessageWarning="green";
         $name = "";
         $email = "";
@@ -166,6 +166,24 @@ if (isset($_POST["submit"])) {
   <title>
     Heficience
   </title>
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1673451336336178');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1"
+             src="https://www.facebook.com/tr?id=1673451336336178&ev=PageView&noscript=1"/>
+    </noscript>
+    <!-- End Facebook Pixel Code -->
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
   <!-- Nucleo Icons -->
