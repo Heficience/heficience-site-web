4ff188748cf8087e9010056bca5a23795e1967cd
Paul WOISARD <paulwoisard@gmail.com>
2022-04-23 18:46:54 +0200

    retrait message captcha ok

--- pages/validate.php
+++ pages/validate.php
@@ -2,7 +2,7 @@
 	session_start();
 	if(isset($_POST["captcha"])&&$_POST["captcha"]!=""&&$_SESSION["code"]==$_POST["captcha"])
 	{
-		$status = "Votre code captcha est correct.";
+		$status = "";
     $messageCaptchaColor = "color: green";
 	}else{
 		$status = "Le code captcha entré ne correspond pas! Veuillez réessayer.";
